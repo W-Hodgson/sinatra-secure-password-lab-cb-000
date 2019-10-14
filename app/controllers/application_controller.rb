ea27b045cbcfd6330a6b3252454b583f314f8e06
Will Hodgson <will.hodgson@current-rms.com>
2019-10-14 14:58:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,8 @@ class ApplicationController < Sinatra::Base
   post "/signup" do
     #your code here
     if params[:username] != "" && params[:password] != ""
+      user = User.new(:username => params[:username], :password => params[:password])
+      user.save
       redirect "/login"
     else
       redirect "/failure"
